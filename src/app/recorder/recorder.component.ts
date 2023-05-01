5a4d23de270573d10627d91e7ead8d131bd5dc56
Viraj Yadav <viraj.yadav@ansell.com>
2023-05-01 14:34:18 +0530

    Added Recorder Component

---
+++ src/app/recorder/recorder.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-recorder',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recorder.component.html',
+  styleUrls: ['./recorder.component.scss']
+})
+export class RecorderComponent {
+
+}
